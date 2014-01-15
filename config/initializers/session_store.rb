fe7f70afafbf34ab49e010709d15c83d4ca9561f
kremez19 <kremez19@gmail.com>
2014-01-15 17:45:23 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EducationalInstitutionSite::Application.config.session_store :cookie_store, key: '_educational_institution_site_session'
